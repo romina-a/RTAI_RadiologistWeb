be7092d238fc185a8974cd74d33e38ea05c202b5
Romina Abadi <rmn@yorku.ca>
2022-03-22 16:13:46 -0400

    annotation handler update for mutliple users

--- annotation_handler.py
+++ annotation_handler.py
@@ -4,64 +4,70 @@ import shutil
 
 # -- -- -- DATA FILE INFO
 DATA_FOLDER = "./data"
-DATA_FILE = "annotations.csv"
 HEADER = ['image_id', 'annotations']
 ID_COLUMN_NAME = 'image_id'
 
-FILE_PATH = os.path.join(DATA_FOLDER, DATA_FILE)
-TEMP_FILE_PATH = os.path.join(DATA_FOLDER, 'annotations_temp.csv')
-
 
 # -- -- CREATE DATA FILE
-def create_data_file():
+def create_data_file(file_path):
     # ensure the DATA_FOLDER exists
     try:
         os.makedirs(DATA_FOLDER)
     except OSError:
         pass
     # ensure the DATA_FILE exists
-    if not os.path.isfile(FILE_PATH):
-        file = open(FILE_PATH, 'a+')
+    if not os.path.isfile(file_path):
+        file = open(file_path, 'a+')
         writer = csv.writer(file)
         writer.writerow(HEADER)
         file.close()
-    # TODO check header exists and if not add header to the file
+    else:
+        pass
+        # TODO check header exists and if not add header to the file
+        # TODO check header matches the header and if not raise error
 
 
-def _add_row(image_id, annotations):
-    inp = open(FILE_PATH, 'a+')
-    writer = csv.writer(inp)
-    writer.writerow([image_id, annotations])
-    inp.close()
+class AnnotationHandler:
+    def __init__(self, user_id):
+        self.user_id = user_id
 
+        data_file = str(user_id)+"_"+"annotations.csv"
+        temp_file = str(user_id)+"_"+"annotations_temp.csv"
+        file_path = os.path.join(DATA_FOLDER, data_file)
+        temp_file_path = os.path.join(DATA_FOLDER, temp_file)
+        self.file_path = file_path
+        self.temp_file_path = temp_file_path
 
-def delete_row(image_id):
-    with open(FILE_PATH, 'r') as inp, open(TEMP_FILE_PATH, 'w+') as temp:
-        csv_reader = csv.DictReader(inp)
-        csv_writer = csv.DictWriter(temp, csv_reader.fieldnames)
-        csv_writer.writeheader()
-        for row in csv_reader:
-            if row['image_id'] != image_id:
-                csv_writer.writerow(row)
+        create_data_file(self.file_path)
+
+    def _add_row(self, image_id, annotations):
+        inp = open(self.file_path, 'a+')
+        writer = csv.writer(inp)
+        writer.writerow([image_id, annotations])
         inp.close()
-        temp.close()
-    shutil.move(TEMP_FILE_PATH, FILE_PATH)
 
+    def delete_row(self, image_id):
+        with open(self.file_path, 'r') as inp, open(self.temp_file_path, 'w+') as temp:
+            csv_reader = csv.DictReader(inp)
+            csv_writer = csv.DictWriter(temp, csv_reader.fieldnames)
+            csv_writer.writeheader()
+            for row in csv_reader:
+                if not (row['image_id'] == image_id):
+                    csv_writer.writerow(row)
+            inp.close()
+            temp.close()
+        shutil.move(self.temp_file_path, self.file_path)
 
-def update_row(image_id, annotations):
-    delete_row(image_id)
-    _add_row(image_id, annotations)
+    def update_row(self, image_id, annotations):
+        self.delete_row(image_id)
+        self._add_row(image_id, annotations)
 
-
-def get_row(column_id):
-    with open(FILE_PATH, 'r') as inp:
-        csv_reader = csv.DictReader(inp)
-        for row in csv_reader:
-            if row[ID_COLUMN_NAME] == column_id:
-                inp.close()
-                return row
-    inp.close()
-    return None
-
-
-create_data_file()
+    def get_row(self, column_id):
+        with open(self.file_path, 'r') as inp:
+            csv_reader = csv.DictReader(inp)
+            for row in csv_reader:
+                if row[ID_COLUMN_NAME] == column_id:
+                    inp.close()
+                    return row
+        inp.close()
+        return None
--- app.py
+++ app.py
@@ -1,32 +1,45 @@
 import json
 import os
-import annotation_handler as an
+
+import annotation_handler
 import user_handler as uh
 
-from flask import Flask, request, render_template, session, flash, redirect, url_for
+from flask import Flask, request, render_template, session, flash, redirect, url_for, Response, abort
 
 app = Flask(__name__)
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
+
+
+@app.errorhandler(401)
+def custom_401(error):
+    return Response('<Only authorized users can save or see the annotations>',
+                    401, {'WWW-Authenticate':'Basic realm="Login Required"'})
+
 
 @app.route('/', methods=['GET', 'POST'])
 def radiologist_page():
     if request.method == 'GET':
         return render_template("index.html")
     if request.method == 'POST':
+        if session['an'] is None:
+            abort(401)
         im = request.json['imageName']
         vertices = request.json['coordinates']
 
-        an.update_row(im, json.dumps(vertices))
+        session['an'].update_row(im, json.dumps(vertices))
         return ''
 
 
 @app.route('/annotations', methods=['POST'])
 def get_annotations():
-    im = request.json['imageName']
-    vertices = an.get_row(im)['annotations']
-    if vertices is None:
+    if session.get('an') is None:
         vertices = "[]"
+    else:
+        im = request.json['imageName']
+        vertices = session.get('an').get_row(im)['annotations']
+        if vertices is None:
+            vertices = "[]"
     return json.dumps(json.loads(vertices))
 
 
@@ -35,13 +48,16 @@ def login():
     """Log in a registered user by adding the user id to the session."""
     if request.method == "POST":
         print("login post")
-        user_id = request.form["username"]
-        password = request.form["password"]
+        user_id = request.form['username']
+        password = request.form['password']
 
-        print("user_id and password", user_id, " ", password)
         error = None
+
+        if session.get('user_id') is not None:
+            error = "Already logged in"
+
         user = uh.get_row(user_id)
-        print("user is", user)
+
         if user is None:
             error = "Incorrect username."
         elif not user["password"] == password:
@@ -51,8 +67,11 @@ def login():
             # store the user id in a new session and return to the index
             print("here")
             session.clear()
-            session["user_id"] = user["user_id"]
+            user_id = user['user_id']
+            session['user_id'] = user_id
+            session['an'] = annotation_handler.AnnotationHandler(user_id)
             return redirect(url_for("radiologist_page"))
+            # return redirect(request.url) TODO try this
 
         flash(error)
 
@@ -63,7 +82,7 @@ def login():
 def logout():
     """Clear the current session, including the stored user id."""
     session.clear()
-    return ("you're logged out!")
+    return "you're logged out!"
 
 
 if __name__ == '__main__':
